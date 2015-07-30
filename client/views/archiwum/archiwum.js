50005f0e0a26c29755ece8eaa3382b2a3a23de22
Damian Kwasny <damian.kwasny@onebi.eu>
2015-07-30 11:35:58 +0200

    odinstalowanie konfliktowej paczki po meteor update

--- client/views/archiwum/archiwum.js
+++ client/views/archiwum/archiwum.js
@@ -21,21 +21,22 @@ Template.archiwum.helpers({
             rowsPerPage: 10,
             showFilter: true,
             showNavigation: 'always',
-            showColumnToggles: true,
+            showColumnToggles: false,
             enableRegex: false,
             fields: [
-                {key: 'dataWprowadzenia', label: "Data", tmpl:Template.dataUtwKwestia},
-                {key: 'kwestiaNazwa', label: "Nazwa Kwestii", tmpl: Template.nazwaKwestiArchiwumLink},
+                {key: 'dataWprowadzenia', label: Template.listKwestiaAdminColumnLabel, labelData: {title: "Data wprowadzenia Kwestii i rozpoczęcia jej deliberacji", text:"Data"}, tmpl:Template.dataUtwKwestia},
+                {key: 'kwestiaNazwa', label: Template.listKwestiaAdminColumnLabel, labelData: {title: "Kliknij, aby zobaczyć szczegóły", text:"Nazwa kwestii"}, tmpl: Template.nazwaKwestiArchiwumLink},
                 {
                     key: 'sredniaPriorytet',
-                    label: "Priorytet",
+                    label: Template.listKwestiaAdminColumnLabel,
+                    labelData: {title: "Kliknij, aby zmienić swój priorytet dla tej Kwestii", text:"Priorytet"},
                     tmpl: Template.priorytetKwestia,
                     sortOrder: 1,
                     sortDirection: 'descending'},
                 {key: 'temat_id', label: "Temat", tmpl: Template.tematKwestia},
                 {key: 'rodzaj_id', label: "Rodzaj", tmpl: Template.rodzajKwestia},
-                {key: 'dataGlosowania', label: "Finał", tmpl: Template.dataGlKwestia},
-                {key: 'status', label: "Status"},
+                {key: 'dataGlosowania', label: Template.listKwestiaAdminColumnLabel, labelData: {title: "Data zakończenia głosowania", text:"Finał"}, tmpl: Template.dataGlKwestia},
+                {key: 'status', label: Template.listKwestiaAdminColumnLabel, labelData: {title: "Etap, na którym znajduje sie ta Kwestia", text:"Status"}},
                 {key: 'options', label: "Opcje", tmpl: Template.editColumnKwestiaArch }
             ]
         };
@@ -57,3 +58,6 @@ Template.archiwum.helpers({
     }
 });
 
+Template.archiwum.rendered = function(){
+    $('[data-toggle="tooltip"]').tooltip();
+}
