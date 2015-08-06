2b464a4cfdb32b9ef76d5cf32c8a9aa977a8a064
Damian Kwasny <damian.kwasny@onebi.eu>
2015-08-06 14:53:54 +0200

    Zmiany w dyskusji wstrzymywania kwestii

--- client/views/user/kwestie/procedura_wstrzymania_discussion.js
+++ client/views/user/kwestie/procedura_wstrzymania_discussion.js
@@ -39,6 +39,9 @@ Template.postForm.events({
         var isParent = true;
         var czyAktywny = true;
         var user_full_name = Meteor.user().profile.full_name;
+        var parentId = null;
+        var ratingValue = null;
+        var glosujacy = [];
 
         var post = [{
             kwestia_suspended_id: kwestia_suspended_id,
@@ -47,7 +50,10 @@ Template.postForm.events({
             user_full_name:user_full_name,
             add_date: add_date,
             isParent: isParent,
-            czyAktywny: czyAktywny
+            czyAktywny: czyAktywny,
+            parentId: parentId,
+            wartoscPriorytetu: ratingValue,
+            glosujacy: glosujacy
         }];
 
         if (isNotEmpty(post[0].kwestia_suspended_id) && isNotEmpty(post[0].post_message) && isNotEmpty(post[0].user_id) &&
@@ -79,6 +85,8 @@ Template.postAnswerForm.events({
         var isParent = false;
         var czyAktywny = true;
         var user_full_name = Meteor.user().profile.full_name;
+        var ratingValue = null;
+        var glosujacy = [];
 
         var post = [{
             kwestia_suspended_id: kwestia_suspended_id,
@@ -87,8 +95,10 @@ Template.postAnswerForm.events({
             user_full_name: user_full_name,
             add_date: add_date,
             isParent: isParent,
+            czyAktywny: czyAktywny,
             parentId: parentId,
-            czyAktywny: czyAktywny
+            wartoscPriorytetu: ratingValue,
+            glosujacy: glosujacy
         }];
 
         if (isNotEmpty(post[0].kwestia_suspended_id) && isNotEmpty(post[0].post_message) &&
@@ -110,3 +120,72 @@ Template.postAnswerForm.events({
     }
 });
 
+Template.postRating.events({
+   'click #ratingButton':function(e){
+
+       var ratingValue = e.target.value;
+       var ratingPostId = e.target.name;
+       var glosujacy = [];
+
+       console.log(ratingValue+" "+ratingPostId);
+
+       var post = KwestiaSuspendedPosts.findOne({_id:ratingPostId});
+
+       var flaga = false;
+       for(var i=0; i < post.glosujacy.length; i++) {
+
+           if(post.glosujacy[i][0] === Meteor.userId) {
+
+               if(post.glosujacy[i][1] === e.target.value) {
+                   throwError("Nadałeś już priorytet o tej wadze w tej kwestii!");
+                   return false;
+               } else if(post.glosujacy[i][1] > e.target.value) {
+
+                   var roznica = post.glosujacy[i][1] - e.target.value;
+                   roznica = -roznica;
+
+                   KwestiaSuspendedPosts.update(ratingPostId, {$set: {glosujacy: [[user._id, liczba]]}, $inc: {wartoscPriorytetu: roznica}});
+                   flaga = true;
+               } else if(post.glosujacy[i][1] < liczba) {
+
+                   var roznica = e.target.value - post.glosujacy[i][1];
+                   Kwestia.update(ratingPostId, {$set: {glosujacy: [[user._id, liczba]]} , $inc: {wartoscPriorytetu: roznica}});
+                   flaga = true;
+               }
+           }
+       }
+       if(flaga === false) {
+
+           var srednia = (kwestia.wartoscPriorytetu + liczba)/(kwestia.glosujacy.length + 1);
+           Kwestia.update(currentKwestiaId, {$addToSet: {glosujacy: [user._id, liczba]}, $inc: {wartoscPriorytetu: liczba}, $set: {sredniaPriorytet: srednia}});
+           flaga = true;
+       }
+
+
+       var postUpdate = [{
+           kwestia_suspended_id: post.kwestia_suspended_id,
+           post_message: post.post_message,
+           user_id: post.user_id,
+           user_full_name: post.user_full_name,
+           add_date: post.add_date,
+           isParent: post.isParent,
+           parentId: post.parentId,
+           czyAktywny: post.czyAktywny,
+           wartoscPriorytetu: ratingValue,
+           glosujacy: glosujacy
+       }]
+
+       if (isNotEmpty(post[0].kwestia_suspended_id) && isNotEmpty(post[0].post_message)) {
+           Meteor.call('updateKwestiaSuspendedPostsRating', post, function (error,ret) {
+               if (error) {
+                   if (typeof Errors === "undefined")
+                       Log.error('Error: ' + error.reason);
+                   else
+                       throwError(error.reason);
+               }else{
+                   console.log("Update zaliczony");
+               }
+           });
+       }
+   }
+});
