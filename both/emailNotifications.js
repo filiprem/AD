246ba1a8e1f6c4939e403dca3a21e6ca8c94aa29
Karolina Salamon <karolina.salamon@onebi.eu>
2015-08-12 07:58:46 +0200

    fix errors i emailNotifications

--- both/emailNotifications.js
+++ both/emailNotifications.js
@@ -1,5 +1,22 @@
 EmailNotifications = function () {
-    this.registerEmailNotification = function(){
+    //Komunikat po dodaniu Kwestii
+    this.registerAddKwestiaNotification = function(systemNazwa, organizacja, user, nazwaKwestii, rodzaj,
+                                                   szczegolyKwestii, linkDK, linkLoginTo){
+    };
 
-    }
+    //Komunikat po rozpoczęciu głosowania
+    this.registerStartGlosowanieNotification = function(systemNazwa, organizacja, user, nazwaKwestii, temat, rodzaj,
+                                                        szczegolyKwestii, final, priorytet, linkDK, silaPrior,
+                                                        kworum, obecnych, linkLoginTo){
+    };
+
+    //Komunikat o podjęciu uchwały
+    this.PodjecieUchwalyNotification = function(systemNazwa, organizacja, user, status, nazwaKwestii,
+                                                temat, rodzaj, szczegolyKwestii, pdfUchwala, linkDR, linkLoginTo){
+    };
+
+    //Lobbowanie Kwestii
+    this.registerLobKwestiaNotification = function(systemNazwa, organizacja, user, kwestiaNazwa, temat, rodzaj,
+                                                   szczegolyKwestii, uzasadnienie, imie, nazwisko, email, linkMojProfil){
+    };
 }
