130a4e0d6c20570eb7810dbfc157b0d47b71a803
Damian Kwasny <damian.kwasny@onebi.eu>
2015-08-13 10:37:38 +0200

    Nadawanie tematu i rodzaju dla kwestii przez admin + walidacja

--- client/views/admin/kwestie/edit_kwestia_oczekujaca_admin.js
+++ client/views/admin/kwestie/edit_kwestia_oczekujaca_admin.js
@@ -1,9 +1,82 @@
+Template.kwestiaOczekujaca.created = function(){
+    this.choosedTopicRV = new ReactiveVar();
+}
+
+Template.kwestiaOczekujaca.rendered = function(){
+    $("#setTopicForm").validate({
+        messages:{
+            tematSelect:{
+                required:fieldEmptyMesssage()
+            },
+            rodzajSelect:{
+                required:fieldEmptyMesssage()
+            }
+        },
+        highlight: function(element) {
+            var id_attr = "#" + $( element ).attr("id") + "1";
+            $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+            $(id_attr).removeClass('glyphicon-ok').addClass('glyphicon-remove');
+        },
+        unhighlight: function(element) {
+            var id_attr = "#" + $( element ).attr("id") + "1";
+            $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+            $(id_attr).removeClass('glyphicon-remove').addClass('glyphicon-ok');
+        },
+        errorElement: 'span',
+        errorClass: 'help-block',
+        errorPlacement: function(error, element) {
+            if(element.length) {
+                error.insertAfter(element);
+            } else {
+                error.insertAfter(element);
+            }
+        }
+    })
+}
 
 Template.kwestiaOczekujaca.helpers({
    'getTematy':function(){
-       return Temat.find({});
+       return Temat.find({},{sort:{nazwaTemat:1}});
    },
     'getRodzaje':function(){
-        return Rodzaj.find({});
+        var self = Template.instance();
+        var idTopic = self.choosedTopicRV.get();
+        return !!idTopic ? Rodzaj.find({idTemat:idTopic},{sort:{nazwaRodzaj:1}}) : null;
+    }
+});
+
+Template.kwestiaOczekujaca.events({
+    'change #tematSelect':function(e){
+        var self = Template.instance();
+        self.choosedTopicRV.set(e.target.value);
+    },
+    'submit form':function(e){
+        e.preventDefault();
+
+        var rodzaj = $(e.target).find('[id=rodzajSelect]').val();
+        var temat = $(e.target).find('[id=tematSelect]').val();
+        var idKwestia = $(e.target).find('[id=idKwestia]').val();
+        var status = KWESTIA_STATUS.DELIBEROWANA;
+        console.log(rodzaj);
+        console.log(temat);
+        console.log(idKwestia);
+
+        var kwestiaUpdate = {
+            idTemat:temat,
+            idRodzaj:rodzaj,
+            status:status
+        };
+
+        Meteor.call('updateKwestiaNoUpsert', idKwestia,kwestiaUpdate, function (error) {
+            if (error) {
+                if (typeof Errors === "undefined")
+                    Log.error('Error: ' + error.reason);
+                else
+                    throwError(error.reason);
+
+            } else {
+                Router.go('listaKwestiiOczekujacych');
+            }
+        });
     }
 });
