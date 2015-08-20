951a10a894ef9c749d5a76d48e5475acc70ad321
ksalamon <karolina.salamon@onebi.eu>
2015-08-20 14:41:53 +0200

    Projektowanie modulu uprawnien, dodanie podstawowych uprawnien

--- server/publications.js
+++ server/publications.js
@@ -18,6 +18,10 @@ Meteor.publish(null, function (){
     return Meteor.roles.find({})
 });
 
+Meteor.publish('subroles', function(){
+    return Subroles.find({});
+})
+
 Meteor.publish('parametr', function() {
     return Parametr.find({});
 });
@@ -97,6 +101,68 @@ Meteor.publish('pagesInfoByLang', function(routeName) {
 });
 
 Meteor.startup(function () {
+    //Subroles.remove({});
+    var permissions = [
+        //uzytkownicy
+        {name: "manage-my-account", description: "Zarządzanie swoim profilem"},
+        {name: "edit-my-profile", description: "Edycja mojego profilu"},
+        {name: "manage-all-profiles", description: "Zarządzanie wszystkimi profilami użytkowników"},
+        {name: "see-user-profile-info", description: "Możliwość przeglądania szczegółów profili użytkowników"},
+        {name: "manage-all-users", description: "Zarządzanie użytkownikami"},
+        {name: "edit-user-profile", description: "Możliwość edycji profilu użytkownika"},
+        {name: "manage-roles", description: "Zarządzanie rolami"},
+        {name: "manage-subroles", description: "Zarządzanie uprawnieniami"},
+        {name: "add-role-permission", description: "Możliwość dodania roli"},
+        {name: "add-subrole-permission", description: "Możliwość dodania uprawnienia"},
+
+        //rodzaje
+        {name: "manage-all-rodzaje", description: "Zarządzanie rodzajami"},
+        {name: "add-rodzaj", description: "Możliwość dodania rodzaju"},
+        {name: "edit-rodzaj", description: "Możliwość edycji rodzaju"},
+
+        //tematy
+        {name: "manage-all-tematy", description: "Zarządzanie tematami"},
+        {name: "add-temat", description: "Możliwość dodania tematu"},
+        {name: "edit-temat", description: "Możliwość edycji tematu"},
+
+        //kwestie
+        {name: "manage-kwestia-list", description: "Zarządzanie Wykazem Kwestii"},
+        {name: "can-vote-kwestia", description: "Możliwość nadawania priorytetu Kwestii"},
+        {name: "can-vote-post", description: "Możliwość głosowania na posty w dyskusjii Kwestii"},
+        {name: "manage-realizacja", description: "Zarządzanie modułem Realizacja"},
+        {name: "manage-archiwum-list", description: "Zarządzanie Archiwum"},
+        {name: "manage-moje-kwestie", description: "Zarządzanie moimi Kwestiami"},
+        {name: "manage-kwestie-oczekujace", description: "Zarządzanie Kwestiami oczekującymi na kategoryzację"},
+        {name: "kategoryzacja-kwestii-oczekujacej", description: "Możliwość kategoryzacji Kwestii"},
+        {name: "manage-kwestia-info", description: "Możliwość podglądu informacji o Kwestii"},
+        {name: "can-add-option", description: "Możliwość dodania Opcji Kwestii"},
+        {name: "can-add-priorytet-kwestia", description: "Możliwość nadania priorytetu Kwestii"},
+        {name: "can-add-priorytet-post", description: "Możliwość nadania priorytetu postu"},
+        {name: "can-add-post-archiwum", description: "Możliwość wybrania Przenieś Do Archiwum"},
+        {name: "can-add-post-kosz", description: "Możliwość wybrania Przenieś Do Kosza"},
+        {name: "can-clear-priorytety", description: "Możliwość wyczyszczenia priorytetów"},
+        {name: "can-add-post", description: "Możliwość dodania posta - dyskusja"},
+        {name: "can-add-answer", description: "Możliwość dodania odpowiedzi do posta - dyskusja"},
+
+        //parametry
+        {name: "manage-all-parametry", description: "Zarządzanie parametrami"},
+        {name: "add-parametr", description: "Możliwość dodania parametru"},
+        {name: "edit-parametr", description: "Możliwość edycji parametru"},
+        {name: "preview-parametr", description: "Możliwość podglądu parametru"},
+
+        //raporty
+        {name: "manage-all-raporty", description: "Zarządzanie raportami"},
+        {name: "add-raport", description: "Możliwość dodania raportu"},
+
+        //języki
+        {name: "manage-all-languages", description: "Zarządzanie językami"},
+        {name: "add-language", description: "Możliwość dodania języka"},
+        {name: "edit-language", description: "Możliwość edycji języka"}
+    ];
+
+    _.each(permissions, function (e) {
+        var s = Subroles.insert({name: e.name, description: e.description});
+    });
     var globalParameters=[
         {
             "nazwaOrganizacji": "Aktywna Demokracja",
