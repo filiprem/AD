61979b8f65d9576baae8b427b64cefa55116bd20
Konrad <konrad@o.pl>
2015-09-04 12:06:02 +0200

    Zmiany dostepow admina oraz dodanie menu usera

--- server/methods/parametr.js
+++ server/methods/parametr.js
@@ -4,19 +4,7 @@ Meteor.methods({
             nazwaOrganizacji: newParametr[0].nazwaOrganizacji,
             terytorium: newParametr[0].terytorium,
             kontakty: newParametr[0].kontakty,
-            regulamin: newParametr[0].regulamin,
-            pktDodanieKwestii: newParametr[0].pktDodanieKwestii,
-            pktDodanieKomentarza: newParametr[0].pktDodanieKomentarza,
-            pktDodanieOdniesienia: newParametr[0].pktDodanieOdniesienia,
-            pktNadaniePriorytetu: newParametr[0].pktNadaniePriorytetu,
-            pktAwansKwestii: newParametr[0].pktAwansKwestii,
-            pktUdzialWZespoleRealizacyjnym: newParametr[0].pktUdzialWZespoleRealizacyjnym,
-            pktZlozenieRaportuRealizacyjnego: newParametr[0].pktZlozenieRaportuRealizacyjnego,
-            pktWycofanieKwestiiDoArchiwum: newParametr[0].pktWycofanieKwestiiDoArchiwum,
-            pktWycofanieKwestiiDoKosza: newParametr[0].pktWycofanieKwestiiDoKosza,
-            pktWyjscieZZespoluRealizacyjnego: newParametr[0].pktWyjscieZZespoluRealizacyjnego,
-            pktBrakUdzialuWGlosowaniu: newParametr[0].pktBrakUdzialuWGlosowaniu,
-            okresSledzeniaWatkuPrzenoszacego:newParametr[0].okresSledzeniaWatkuPrzenoszacego
+            regulamin: newParametr[0].regulamin
         });
     },
     updateParametr: function(id, parametr){
